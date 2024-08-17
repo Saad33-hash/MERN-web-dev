95ab5476ffade1b2c9ab2241682e3cd2dfef0912
saad tariq <saadtariq867@gmail.com>
2024-08-18 01:50:13 +0500

    Add files via upload

---
+++ 39-function in javascript/index.js
@@ -0,0 +1,51 @@
+// rpinting anme using function
+/*
+function printName(){
+    console.log("saad");
+}
+printName();
+*/
+
+// printing counting using function
+/*   
+function getCount(n){
+    for(let a=1;a<=n;a++){
+        console.log(a);
+    }
+}
+getCount(10);
+*/
+
+//average
+/*  
+function getaverage(a1,a2){
+    let avg=(a1+a2)/2;
+    console.log(avg);
+}
+getaverage(3,4);
+*/
+
+//printing sum
+/*
+function getSum(s1,s2){
+    let sum=s1+s2;
+    console.log(sum);
+}
+getSum(4,8);
+*/
+
+//myltiplicxarion of two numbers
+/*
+function multiplication(m1,m2){
+    let mul=m1*m2;
+    console.log(mul);
+}
+multiplication(4,6);
+*/
+
+// return functions
+function printName(firstName){
+    return firstName;
+}
+let n=printName("saad");
+console.log(n);
