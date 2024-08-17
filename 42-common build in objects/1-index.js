ff3cfd9e51c68f2a5bbc9bd3929935e34afd9125
saad tariq <saadtariq867@gmail.com>
2024-08-18 01:52:31 +0500

    Add files via upload

---
+++ 42-common build in objects/1-index.js
@@ -0,0 +1,27 @@
+// common build in objects
+/*
+// value of PI
+console.log(Math.PI);
+// Maximum number
+console.log(Math.max(34,56,12,34,00,98));
+//Minimum number
+console.log(Math.min(4,2,5,7,9,1,3,56,0));
+//square root
+console.log(Math.sqrt(10));
+//random number
+console.log(Math.random());
+//power
+console.log(Math.pow(5,3));
+//eulers number
+console.log(Math.E); // give eulers number
+//floor:it gives the mallest integer backwards
+console.log(Math.floor(1.9));
+//ceil:it gives the larget integer rightwards
+console.log(Math.ceil(1.9));
+// give natural log value
+console.log(Math.LN2); // give natural log of 2
+//natural log of 10
+console.log(Math.LN10);
+//absolute value
+console.log(Math.abs(-9));
+*/
