8bfd45b3e308b02457e5e2ee7fcd30be07a6afc8
saad tariq <saadtariq867@gmail.com>
2024-08-18 01:38:16 +0500

    Add files via upload

---
+++ 35-variable and datatype/index.js
@@ -0,0 +1,33 @@
+// let num=1.345;
+// num="saad";
+// num=true;
+// console.log(num);
+
+// let age;
+// console.log(age);
+
+// let age=null;
+// console.log(age);
+
+// saving data bigger than 2^53-1
+// let marks=1234445576234564746448392920202937;
+// console.log(marks);
+
+// to check which type of data is stored we use typeof
+// let marks=null;
+// console.log(typeof(marks));  object type of data
+
+// boolean type of data
+// let marks=true;
+// console.log(typeof(marks));
+
+//number type of data
+// let age=3;
+// console.log(age);
+// function solve(){
+//     var age=12;
+// }
+// console.log(age);  var varaible define inside function can only be accessed there.it can be accessed globally
+
+
+
