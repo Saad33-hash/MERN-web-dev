4ca2a8c36a8b6b6f5fb91d06f3bbd800f1a1ac2c
saad tariq <saadtariq867@gmail.com>
2024-08-18 01:48:59 +0500

    Add files via upload

---
+++ 37-loops and strings/index.js
@@ -0,0 +1,136 @@
+// printing saad 10 times
+/* for(let a=1;a<=10;a++){
+   console.log("a");
+ }  */
+
+// printing counting from 1 to 5
+/* for(let a=1;a<=5;a++){
+    console.log(a);
+} */
+
+// printing reverse counting
+/* for(let a=10;a>0;a--){
+    console.log(a);
+} */
+
+// using break statement
+/* for(let a=1;a<=5;a++){
+   if(a == 3){
+    break;
+   }
+   else{
+    console.log(a);
+   }
+} */
+
+// using continue statement
+/*
+for(let a=1;a<=5;a++){
+    if(a == 3){
+        continue;
+    }
+    else{
+        console.log(a);
+    }
+}
+*/
+
+// printing counting from 1 to 10 usi g while loop
+/* let a=1;
+while(a<=10){
+    console.log(a);
+    a++;
+}
+*/
+
+// priting name 10 times using while
+/* let a=1;
+while(a<=10){
+    console.log("saad");
+    a++;
+}*/
+
+// sum of first five integers
+/*
+ let a=1;
+let sum=0;
+while(a<=5){
+    sum=sum+a;
+    a++;
+}
+console.log(sum); 
+*/
+
+// reverse counting from 5 to 1
+/*
+ let a=5;
+while(a>0){
+    console.log(a);
+    a--;
+}
+*/
+// declaring string
+/*
+let name1="saad";
+console.log(name1);
+*/
+
+// another way
+/*
+let name1='saad';
+console.log(name1); we can also use space and it will print fine.but when we use enter it will throw an error.
+*/
+
+ // string by using enter
+ /*
+let name1=`saad
+tariq`;
+console.log(name1);
+*/
+
+// another way of declaring string
+/*
+let name1=new string(saad);
+console.log(name1);
+*/
+
+
+
+
+// string conctenation
+/*
+let firstName="saad";
+let secondName="Tariq";
+let thirdName=firstName + secondName;
+console.log(thirdName);
+*/
+
+/*  adding two string without using plus(+) sign
+let n1="saad";
+let n2="tariq";
+let n3=${n1} and ${n2};
+console.log(n3);
+*/
+
+/* length of string 
+let op1='saadtariq';
+console.log((op1.length));
+*/
+
+/* convert into uppercase
+ let n1="ali";
+ console.log(n1.toUpperCase());
+ */
+
+/* convert into lowercase 
+let n2="ALI";
+console.log(n2.toLowerCase());
+*/
+
+/* substring in a string */
+/*
+let n1="ridatariq";
+console.log(n1.substring(2,3)); ir will not print the last value.
+console.log(n1.substring(1));   it will start pring from first
+*/
+
