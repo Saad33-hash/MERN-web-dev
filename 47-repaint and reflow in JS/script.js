7407d7d27abbbdf54e4e9b738b1288b933f155fa
saad tariq <saadtariq867@gmail.com>
2024-09-10 14:11:23 +0500

    Add files via upload

---
+++ 47-repaint and reflow in JS/script.js
@@ -0,0 +1,7 @@
+// for(let a=1;a<=100;a++){
+ 
+//     let   para=document.createElement("p");
+//       para.textcontent="this is para " + a;
+//       document.body.appendChild(para);
+//     }
+    console.log("babbar");
