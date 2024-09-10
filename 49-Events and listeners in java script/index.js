a6f7a0d3fa70f3abe3d05ab331a388426c7c4985
saad tariq <saadtariq867@gmail.com>
2024-09-10 14:14:17 +0500

    Add files via upload

---
+++ 49-Events and listeners in java script/index.js
@@ -0,0 +1,7 @@
+
+function changeName(){
+    let fpara=document.querySelector("#fpara"); 
+    fpara.textContent="saad king";
+}
+let fpara=document.querySelector("#fpara");
+fpara.addEventListener("click",changeName());
